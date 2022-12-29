bdfd55dd5f10ca1b821612a92baef95ffd27c5ae
Rafaeltans <102778700+Rafaeltans@users.noreply.github.com>
2022-12-29 10:52:29 +0700

    Update Splash

--- app/src/main/java/com/tugasakhirpab2/rjn/activities/SplashActivity.java
+++ app/src/main/java/com/tugasakhirpab2/rjn/activities/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.tugasakhirpab2.rjn.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,11 +8,23 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.tugasakhirpab2.rjn.R;
 import com.tugasakhirpab2.rjn.databinding.ActivitySplashBinding;
+import com.tugasakhirpab2.rjn.model.User;
 
 public class SplashActivity extends AppCompatActivity {
     private ActivitySplashBinding binding;
+    private String userId;
+    private String mUsername;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mRoot, mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +33,32 @@ public class SplashActivity extends AppCompatActivity {
         binding = ActivitySplashBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, SignUpActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, 3000);
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() == null) {
+            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(SplashActivity.this, SignUpActivity.class));
+                    finish();
+                }
+            }, 3000);
+        }else {
+            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }
+            }, 3000);
+        }
+
+//        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent(SplashActivity.this, SignUpActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        }, 3000);
     }
 }
