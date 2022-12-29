90062e6f2aa26aae4853cac312a4378c3e0edcfc
Rafaeltans <102778700+Rafaeltans@users.noreply.github.com>
2022-12-29 10:18:08 +0700

    Reset Pass

--- app/src/main/java/com/tugasakhirpab2/rjn/activities/ResetPassActivity.java
+++ app/src/main/java/com/tugasakhirpab2/rjn/activities/ResetPassActivity.java
@@ -1,16 +1,75 @@
 package com.tugasakhirpab2.rjn.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.tugasakhirpab2.rjn.R;
+import com.tugasakhirpab2.rjn.databinding.ActivityResetPassBinding;
 
 public class ResetPassActivity extends AppCompatActivity {
+    private ActivityResetPassBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reset_pass);
+
+        binding = ActivityResetPassBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.btnResetPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.etEmail.getText().toString();
+
+                if(TextUtils.isEmpty(email))
+                {
+                    binding.etEmail.setError("Enter your email address!");
+                    return;
+                }
+
+                mAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful())
+                                {
+                                    Toast.makeText(ResetPassActivity.this, "We have sent instructions to reset your password", Toast.LENGTH_SHORT).show();
+                                }
+                                else
+                                {
+                                    Toast.makeText(ResetPassActivity.this, "Failed to send reset email", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        binding.btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ResetPassActivity.this, SignInActivity.class));
+                finish();
+            }
+        });
+
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ResetPassActivity.this, SignUpActivity.class));
+                finish();
+            }
+        });
     }
 }
